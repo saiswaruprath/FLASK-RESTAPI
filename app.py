5d07d6ab2fc3744e383d8b618f31516d5b079881
Sai Swarup Rath <saiswaruprath7@gmail.com>
2022-07-04 13:00:11 -0700

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,15 @@ def use_parameter():
     else:
         return jsonify(message='Welcome ' + name + 'You are a bud')
 
+    
+    
+@app.route('/url_variables/<string:name>/<int:age>')
+def url_variables(name: str, age: int):
+    if age < 18:
+        return jsonify(message="Sorry " + name + ", you are not old enough."), 401
+    else:
+        return jsonify(message="Welcome " + name + ", you are old enough!")    
+    
 
 if __name__ == '__main__':
     app.run()
