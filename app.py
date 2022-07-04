2dbac1811f46fdab4448ab0ea1764f86c8dc3814
Sai Swarup Rath <saiswaruprath7@gmail.com>
2022-07-04 11:37:48 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+@app.route('/super_simple')
+def super_simple():
+    return 'Hello from the planetary API!!!'
+
+
+if __name__ == '__main__':
+    app.run()
