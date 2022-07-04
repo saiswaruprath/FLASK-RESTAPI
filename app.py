86ebfd316cca1b9444dd8bf351228020f4d551d9
Sai Swarup Rath <saiswaruprath7@gmail.com>
2022-07-04 11:48:35 -0700

    Update app.py

--- app.py
+++ app.py
@@ -13,5 +13,10 @@ def super_simple():
     return jsonify(message='Hello from the planetary API!!!')
 
 
+
+@app.route('/not_found')
+def not_found():
+    return jsonify(message='sorry no data found'), 404.             ###adding 404 status code to show in postman once we trigger not found endpoint
+
 if __name__ == '__main__':
     app.run()
