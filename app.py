e0333d75490ecad69c00b83fe9a8fa2ef7e7cbc0
Sai Swarup Rath <saiswaruprath7@gmail.com>
2022-07-06 23:27:55 -0700

    Update app.py

--- app.py
+++ app.py
@@ -123,6 +123,15 @@ class Planet(db.Model):
     radius = Column(Float)
     distance = Column(Float)
 
+    
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'first_name', 'last_name', 'email', 'password')
+
+
+class PlanetSchema(ma.Schema):
+    class Meta:
+        fields = ('planet_id', 'planet_name', 'planet_type', 'home_star', 'mass', 'radius', 'distance')    
 
 if __name__ == '__main__':
     app.run()
