1180faf73e3c77aaf1fe56fec75e880285fbb190
Sai Swarup Rath <saiswaruprath7@gmail.com>
2022-07-06 23:23:05 -0700

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,19 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'pl
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
+
+@app.cli.command('db_create')
+def db_create():
+    db.create_all()
+    print('Database created!')
+
+
+@app.cli.command('db_drop')
+def db_drop():
+    db.drop_all()
+    print('Database dropped!')
+    
+
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
